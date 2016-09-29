bc67d9c76e5363a2a7e21b03dd07a79b9a0c2510
Travis Hubbard <thubbard@sugarcrm.com>
2016-09-29 12:19:10 -0700

    added listening

--- custom/views/ptr/ptr-layout.js
+++ custom/views/ptr/ptr-layout.js
@@ -20,8 +20,18 @@ var PtrCustomView = customization.declareView({
     template: 'ptr-custom-view',
     shouldFetchContext: false,
 
+    audioContext: undefined,
+    recorder: undefined,
+
     initialize: function(options) {
         this._super(options);
+
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+        this.audioContext = new AudioContext;
+
+
+        console.log('this.audioContext: ', this.audioContext);
 
         // Add ptr to current view and attach handler.
         ptr({
@@ -30,10 +40,35 @@ var PtrCustomView = customization.declareView({
         });
     },
 
-    events: {
-        "click #startFauxSiri": 'startSiriListening',
-        "click #stopFauxSiri": 'stopSiri',
+    _render: function() {
+        this._super('_render');
 
+        this.$('#startFauxSiri').off('click', null, this);
+        this.$('#stopFauxSiri').off('click', null, this);
+
+        this.$('#startFauxSiri').on('click', _.bind(this.startSiriListening, this));
+        this.$('#stopFauxSiri').on('click', _.bind(this.stopSiri, this));
+    },
+
+    /**
+     *
+     *
+     */
+    startSiriListening: function() {
+        this.$("#startFauxSiri").hide();
+        this.$("#stopFauxSiri").show();
+        this.$("#spinnerVoice").show();
+
+        app.alert.show('siri_listening', {
+            messages: ['Listening....'],
+            closeable: true,
+            autoClose: false,
+            level: 'load',
+        });
+
+        navigator.getUserMedia({audio: true}, _.bind(this.startRecording, this), function(e) {
+            console.log('No live audio input: ' + e);
+        });
     },
 
     /**
@@ -44,8 +79,9 @@ var PtrCustomView = customization.declareView({
         console.log("Stopping siri.....");
 
         //CleanUp The UI
-        $("#startFauxSiri").hide();
-        $("#stopFauxSiri").hide();
+        this.$("#startFauxSiri").show();
+        this.$("#stopFauxSiri").hide();
+        this.$("#spinnerVoice").hide();
 
         app.alert.dismiss('siri_listening');
 
@@ -56,14 +92,17 @@ var PtrCustomView = customization.declareView({
             level: 'info',
         });
 
+        this.stopRecording();
+
         //Make the request to wit and then create a new account.
-        var that = this;
+
+  /*      var that = this;
         this.sendRequestToWit(null,function(witResponse){
 
             var accountName = witResponse.entities.noun[0].value;
             that.createAccount(accountName);
         })
-
+*/
     },
 
   /**
@@ -72,15 +111,18 @@ var PtrCustomView = customization.declareView({
    * @param err
    * @param callback
    */
-    sendRequestToWit: function(err, callback) {
-
+    sendRequestToWit: function(err, callback, mp3Data) {
         $.ajax({
-            url: 'https://api.wit.ai/message?v=20160928&q=create%20account%20salesforce',
+            url: 'https://api.wit.ai/speech?v=20160928',
+            type: 'POST',
+            processData: false,
+            data: mp3Data,
             headers: {
-                Authorization: 'Bearer X46FYEZ5LOGGH73ZLDC7EV3IYJEGPSY7'
+                Authorization: 'Bearer X46FYEZ5LOGGH73ZLDC7EV3IYJEGPSY7',
+                'Content-type': 'audio/mpeg3'
             },
             success: function(resp) {
-
+                console.log('resp: ', resp);
                 callback(resp);
             },
             error: function() {
@@ -120,32 +162,33 @@ var PtrCustomView = customization.declareView({
 
     },
 
-    /**
-     *
-     *
-     */
-    startSiriListening: function() {
+    startRecording: function(stream) {
+        var input = this.audioContext.createMediaStreamSource(stream);
 
-        $("#startFauxSiri").hide();
-        $("#stopFauxSiri").show();
-        $("#spinnerVoice").show();
-
-
-        app.alert.show('siri_listening', {
-            messages: ['Listening....'],
-            closeable: true,
-            autoClose: false,
-            level: 'load',
+        this.recorder = new Recorder(input, {
+            numChannels: 1
         });
-
-        /*
-        audioinput.start({
-            // Here we've changed the bufferSize from the default to 8192 bytes.
-            bufferSize: 8192
-        });
-        */
-
+        console.log('Recorder initialised.');
+        this.recorder && this.recorder.record();
     },
+
+    stopRecording: function() {
+        this.recorder.stop();
+
+        this.recorder.getBuffer(_.bind(this.getBufferCb, this));
+
+        this.recorder.clear();
+    },
+
+    getBufferCb: function(buffer) {
+        var currentMp3Blob = new Blob([new Uint8Array(buffer[0]) ], {type: 'audio/mp3'});
+
+        console.log('currentMp3Blob: ', currentMp3Blob);
+
+        this.sendRequestToWit(null, _.bind(this.parseResponse, this), currentMp3Blob);
+    },
+
+
     ptrRefresh: function() {
         dialog.showAlert('event has been triggered');
 
@@ -155,6 +198,70 @@ var PtrCustomView = customization.declareView({
 
     loadData: function() {
         // custom data loading method.
+    },
+
+    parseResponse: function(response) {
+        debugger
+        var msgId = response.msg_id;
+        var text = response._text;
+        var entities = response.entities;
+        var intent = entities && entities.intent && entities.intent[0].value;
+        var moduleName = entities && entities.module && entities.module[0].value;
+        var value = entities && entities.noun && entities.noun[0].value;
+
+        console.log('intent: ', intent);
+        console.log('moduleName: ', moduleName);
+        console.log('value: ', value);
+
+        if (intent && moduleName && value) {
+            switch(intent) {
+                case 'create':
+                    this._processCreate(intent, moduleName, value);
+                    break;
+                case 'find':
+                    this._processFind(intent, moduleName, value);
+                    break;
+                case 'get directions':
+                    this._processGetDirections(intent, moduleName, value);
+                    break;
+                case 'call':
+                    this._processCall(intent, moduleName, value);
+                    break;
+            }
+        }
+    },
+
+    _processCreate: function(intent, moduleName, value) {
+        debugger;
+        var beanModuleName = _moduleMap[moduleName];
+        var bean = app.data.createBean(beanModuleName, {
+            name: value,
+            assigned_user_id: app.user.get('id'),
+            assigned_user_name: app.user.get('full_name')
+        });
+
+        app.alert.show('mobilehack_save_start', {
+            messages: ['Saving ' + beanModuleName + ' record: ' + value],
+            closeable: true,
+            autoClose: true,
+            level: 'info'
+        });
+
+        app.api.records('create', beanModuleName, bean.toJSON(), null, {
+            success: _.bind(this._onCreateSuccess, this)
+        });
+    },
+
+    _onCreateSuccess: function(record) {
+        app.alert.dismiss('mobilehack_save_start');
+
+        app.alert.show('mobilehack_save_success', {
+            messages: ['Saved ' + record._module + ' record: ' + record.name],
+            closeable: true,
+            autoClose: true,
+            level: 'success'
+        });
+        app.controller.navigate('#' + record._module + '/' + record.id)
     },
 });
 
